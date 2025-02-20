93ae8f8dd99a6eab7eba5056d884468b37f1d31a
benjaminfink <benhfink@gmail.com>
2025-02-20 14:57:32 -0500

    Added animations and enter button.

--- app.js
+++ app.js
@@ -13,9 +13,37 @@ function runTime() {
         tOD = 'AM';
         hour += 12
     }
-    const minute = t.getMinutes();
-
+    let minute = t.getMinutes();
+    if (minute < 10) {
+        minute = `0${minute}`;
+    }
     document.getElementById('current-time').textContent = `${hour}:${minute} ${tOD}`;
 }
 
 setInterval(runTime, 1);
+
+let letterIndex = 0;
+let letterAddInterval
+
+function typeOutHeader() {
+    const heading = document.getElementById('main-heading');
+    const subheading = document.getElementById('main-subheading');
+    const cursor = document.getElementById('imitation-cursor');
+    const name = ['B', 'e', 'n', ' ', 'F', 'i', 'n', 'k']
+    if (letterIndex < 8) {
+        heading.textContent += name[letterIndex];
+        cursor.style.width = '5px';
+        cursor.style.height = '90px';
+        letterIndex += 1;
+    } else {
+        clearInterval(letterAddInterval);
+        letterAddInterval = undefined;
+        cursor.style.height = '0';
+        cursor.style.width = '0';
+        subheading.style.color = 'black';
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    letterAddInterval = setInterval(typeOutHeader, 100);
+})
